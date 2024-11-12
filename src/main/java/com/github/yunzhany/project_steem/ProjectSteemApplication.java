99f3b4f535b88f0bd5690aa6fd549ccfc7f5213a
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-11 23:03:51 -0600

    Transferring the project to Github starting today...

---
+++ src/main/java/com/github/yunzhany/project_steem/ProjectSteemApplication.java
@@ -0,0 +1,13 @@
+package com.github.yunzhany.project_steem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSteemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSteemApplication.class, args);
+    }
+
+}
