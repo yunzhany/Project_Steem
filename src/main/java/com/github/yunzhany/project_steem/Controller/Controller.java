edbd9ddfe4d46f5c5f398f6e01e7821650c54859
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-12 23:24:37 -0600

    Test Controller

--- src/main/java/com/github/yunzhany/project_steem/Controller/Controller.java
+++ src/main/java/com/github/yunzhany/project_steem/Controller/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping("/hello")
+    @RequestMapping("hello")
     public String hello(){
         System.out.println("Hello World");
         return "hello";
