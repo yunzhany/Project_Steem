073c6d6d7abe218829798e6801648bbb80ddcba9
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-14 23:20:07 -0600

    Frontend-backend separation

--- project_steem_backend/src/main/java/com/github/yunzhany/project_steem/controller/Controller.java
+++ project_steem_backend/src/main/java/com/github/yunzhany/project_steem/controller/Controller.java
@@ -1,7 +1,6 @@
-package com.github.yunzhany.project_steem.Controller;
+package com.github.yunzhany.project_steem.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
