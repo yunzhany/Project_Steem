72e6a6a6f510f5a9f2a61296df0b0008ae384647
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-14 23:32:19 -0600

    Including front-end sub-project

---
+++ project_steem_backend/src/test/java/com/github/yunzhany/project_steem/ProjectSteemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yunzhany.project_steem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSteemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
