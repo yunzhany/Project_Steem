45cfc1c64ce62d90f11b984a0a94bd0ac0fd8212
yunzhany <69857969+yunzhany@users.noreply.github.com>
2024-11-14 23:35:48 -0600

    Let's see if front end is now up...

--- project_steam_frontend/src/components/HelloWorld.vue
+++ project_steam_frontend/src/components/HelloWorld.vue
@@ -14,6 +14,7 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+
     </h3>
   </div>
 </template>
